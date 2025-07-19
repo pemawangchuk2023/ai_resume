361b9bb813aedcdc8efe91e89932974a4130fd08
Pema Wangchuk <pemawangchug1986@gmail.com>
2025-07-19 13:24:58 +0600

    update on deployment

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,11 +1,26 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({
 	resume: { id, companyName, jobTitle, feedback, imagePath },
 }: {
 	resume: Resume;
 }) => {
+	const { fs } = usePuterStore();
+	const [resumeUrl, setResumeUrl] = useState("");
+
+	useEffect(() => {
+		const loadResume = async () => {
+			const blob = await fs.read(imagePath);
+			if (!blob) return;
+			let url = URL.createObjectURL(blob);
+			setResumeUrl(url);
+		};
+
+		loadResume();
+	}, [imagePath]);
 	return (
 		<Link
 			to={`/resume/${id}`}
@@ -20,15 +35,17 @@ const ResumeCard = ({
 					<ScoreCircle score={feedback.overallScore} />
 				</div>
 			</div>
-			<div className='gradient-border animate-in fade-in duration-1000'>
-				<div className='w-full h-full'>
-					<img
-						src={imagePath}
-						alt='resume'
-						className='w-full h-[350px] max-sm:h-[200px] object-cover'
-					/>
+			{resumeUrl && (
+				<div className='gradient-border animate-in fade-in duration-1000'>
+					<div className='w-full h-full'>
+						<img
+							src={resumeUrl}
+							alt='resume'
+							className='w-full h-[350px] max-sm:h-[200px] object-cover object-top'
+						/>
+					</div>
 				</div>
-			</div>
+			)}
 		</Link>
 	);
 };
